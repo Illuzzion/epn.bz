7796bb72c7c8517dcc86f055554aec51bf192486
Illuzzion <79fz270704@gmail.com>
2017-08-17 11:33:56 +0300

    sitemap_split changed to slice

--- sitemap_split.py
+++ sitemap_split.py
@@ -2,6 +2,8 @@
 import argparse
 from lxml import etree
 
+MAX_SITEMAP_URLS = 50000
+
 
 def parse_args():
     args = argparse.ArgumentParser()
@@ -20,8 +22,36 @@ def normalize_node_name(node):
 
 def get_url_text(url_node):
     url_tag = normalize_node_name(url_node).tag
-    params = ["<{tag}>{text}</{tag}>".format(tag=normalize_node_name(param).tag, text=param.text) for param in url]
-    return "<{tag}>\n{inner}\n</{tag}>".format(tag=url_tag, inner="\n".join(params))
+    params = ["<{tag}>{text}</{tag}>".format(tag=normalize_node_name(param).tag, text=param.text) for param in url_node]
+    return "<{tag}>\n{inner}\n</{tag}>\n".format(tag=url_tag, inner="\n".join(params))
+
+
+def gen_sitemap_index(count, baseurl, filename='sitemap_index.xml'):
+    INDEX_BEGIN = '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    INDEX_END = '</sitemapindex>'
+    baseurl = baseurl if baseurl.endswith('/') else baseurl + '/'
+
+    with open(filename, mode='w') as f:
+        f.write(INDEX_BEGIN)
+        for i in range(count):
+            f.write(
+                '<sitemap>\n<loc>{baseurl}sitemap{index}.xml</loc>\n</sitemap>\n'.format(baseurl=baseurl, index=i + 1)
+            )
+
+        f.write(INDEX_END)
+
+
+def gen_sitemap_part(url_list, name):
+    SITEMAP_BEGIN = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
+                    'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" ' \
+                    'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 ' \
+                    'http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n'
+    SITEMAP_END = '</urlset>'
+    with open(name, mode='w') as f:
+        f.write(SITEMAP_BEGIN)
+        for url in url_list:
+            f.write(get_url_text(url))
+        f.write(SITEMAP_END)
 
 
 if __name__ == '__main__':
@@ -36,31 +66,21 @@ if __name__ == '__main__':
         print 'found', len(root), 'urls'
 
         if not all_args.parts:
-            all_args.parts = (len(root) / 50000) + 1
+            all_args.parts = (len(root) / MAX_SITEMAP_URLS) + 1
         print 'using parts:', all_args.parts
 
         urls_in_sitemap = len(root) / all_args.parts
 
         print 'urls_in_sitemap: %s' % urls_in_sitemap
 
-        splited_urls = [list() for _ in range(all_args.parts)]
+        slice_list = [slice(i * urls_in_sitemap, (i + 1) * urls_in_sitemap) for i in range(all_args.parts)]
+        # заменим последний слайс и включим в него все оставшиеся записи
+        slice_list[-1] = slice(slice_list[-1].start, len(root))
 
-        for index, url in enumerate(root):
-            part = index / urls_in_sitemap
-            if part >= all_args.parts:
-                print 'remainder', len(root) % all_args.parts
-                break
-            splited_urls[part].append(get_url_text(url))
+        gen_sitemap_index(len(slice_list), baseurl='http://yandex.ru')
 
-        # остаток
-        # if len(root) % all_args.parts:
-        #     for url in root[index:]:
-        #         splited_urls[part].append(get_url_text(url))
-
-
-
-
-
+        for index, cur_slice in enumerate(slice_list):
+            gen_sitemap_part(root[cur_slice], name='sitemap%d.xml' % (index + 1))
 
     except Exception as e:
         print e.args
