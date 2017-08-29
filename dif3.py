a5e1dee52bc08124e3a61c643bca0b5a8f617b1e
Illuzzion <79fz270704@gmail.com>
2017-08-29 15:09:04 +0300

    some changes in dif3.py

--- dif3.py
+++ dif3.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+from __future__ import print_function
+
 import difflib
 import string
 
@@ -40,9 +42,28 @@ def htmlstruct2list(html_list, text_replacement='$$$'):
     return [lel if lel.startswith('<') and lel.endswith('>') else text_replacement for lel in html_list]
 
 
+def show_dif(a_list, b_list):
+    difres = difflib.SequenceMatcher(None, a_list, b_list)
+    print('match: {}%\n'.format(difres.ratio() * 100))
+
+    for el in difres.get_opcodes():
+        a_result = slice(el[1], el[2])
+        b_result = slice(el[3], el[4])
+
+        if el[0] is not 'equal':
+            print(a_list[el[1] - 1: el[2] + 1])
+            print(b_list[el[3] - 1: el[4] + 1])
+
+            print("{result}\na={a_res}\nb={b_res}\n".format(
+                result=el[0],
+                a_res=" ".join(a_list[a_result]),
+                b_res=" ".join(b_list[b_result])
+            ))
+
+
 if __name__ == '__main__':
-    a_fname = '1.html'
-    b_fname = '2.html'
+    a_fname = '3.html'
+    b_fname = '4.html'
 
     with open(a_fname) as f:
         a_list = html2list(f.read())
@@ -53,16 +74,5 @@ if __name__ == '__main__':
     a_struct = htmlstruct2list(a_list)
     b_struct = htmlstruct2list(b_list)
 
-    difres = difflib.SequenceMatcher(None, a_struct, b_struct)
-    print difres.ratio()
-
-    for el in difres.get_opcodes():
-        a_result = slice(el[1], el[2])
-        b_result = slice(el[3], el[4])
-        # print 'raw result', el
-        if el[0] is not 'equal':
-            print "{result}\na={a_res}\nb={b_res}".format(
-                result=el[0],
-                a_res=" ".join(a_struct[a_result]),
-                b_res=" ".join(b_struct[b_result])
-            )
+    show_dif(a_struct, b_struct)
+    show_dif(a_list, b_list)
